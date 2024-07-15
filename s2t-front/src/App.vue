0534e28240856621134db625219681bb6daeae32
Noé Favier <noe.favier@axopen.com>
2024-07-15 14:58:43 +0200

    feat(front): init front

---
+++ s2t-front/src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="32px" height="32px" />
+    <RouterLink to="/">Dashboard</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+template {
+  width: 99vw;
+  height: 100vh;
+}
+
+nav {
+  text-align: center;
+  display: flex;
+  justify-content: flex-start;
+  gap: 2vh;
+  width: inherit;
+  height: 4vh;
+  padding-left: 3vw;
+  align-items: center;
+  border-radius: 16px;
+  border-bottom: 1px solid #ccc;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  width: calc(100% - 16px);
+  padding: 1vh;
+}
+
+RouterLink {
+  background-color: aqua;
+}
+</style>
---
+++ s2t-front/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+let itemList = [];
+fetch('https://testapi.jasonwatmore.com/products/1')
+  .then(response => response.json())
+  .then(data => itemList = data);
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
